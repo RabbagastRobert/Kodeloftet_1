57d41fe0f2d0ea283d7789a476d635134a7aa873
RabbagastRobert <robertandrelarsen@gmail.com>
2025-08-12 12:58:31 +0200

    Feature: Increased difficulty of using page

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('Hello!');
+const mainElement = document.body.querySelector('main');
+const mainHeader = mainElement.querySelector('h1');
+const mainHeaderText = mainHeader.innerText;
+
+setInterval(() => {
+    let splitText = mainHeaderText.toLowerCase().split('');
+    let wonkyText = '';
+    for (let i = 0; i < splitText.length; i++) {
+        if (Math.random() < 0.8) {
+            wonkyText += splitText[i];
+        } else {
+            wonkyText += splitText[i].toUpperCase();
+        }
+    }
+    mainHeader.innerText = wonkyText;
+}, 250);
